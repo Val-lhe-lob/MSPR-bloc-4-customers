dca80f2f45bc5a1d1572d86ce4d47c8e4a7bad78
Val-lhe-lob <valentin.lobraico@gmail.com>
2025-07-02 23:26:33 +0200

    feat(customers): general token

--- MSPR-BLOC4-Clients-tests/CustomWebApplicationFactory.cs
+++ MSPR-BLOC4-Clients-tests/CustomWebApplicationFactory.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -13,35 +14,22 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 
         builder.ConfigureServices(services =>
         {
-            // Trouve et retire la configuration précédente du DbContext avec SQL Server
+            // Replace DbContext with InMemory
             var descriptor = services.SingleOrDefault(
                 d => d.ServiceType == typeof(DbContextOptions<ClientDBContext>));
-
             if (descriptor != null)
-            {
                 services.Remove(descriptor);
-            }
 
-            // Ajoute le DbContext avec InMemory pour les tests
             services.AddDbContext<ClientDBContext>(options =>
             {
                 options.UseInMemoryDatabase("TestDb");
             });
 
-            // Crée la base et ajoute éventuellement des données de test si besoin
-            var sp = services.BuildServiceProvider();
-
-            using (var scope = sp.CreateScope())
-            {
-                var scopedServices = scope.ServiceProvider;
-                var db = scopedServices.GetRequiredService<ClientDBContext>();
-
-                db.Database.EnsureCreated();
-
-                // Optionnel : seed des données pour tests
-                // db.Clients.Add(new Client { Nom = "TestClient", ... });
-                // db.SaveChanges();
-            }
+            //  Add Fake Authentication for integration tests
+            services.AddAuthentication("Test")
+                .AddScheme<AuthenticationSchemeOptions, FakeJwtAuthHandler>(
+                    "Test", options => { });
         });
     }
 }
+
