7a918d9d5440d81e15d0cd9c0c36bb2e98e6bb70
Val-lhe-lob <valentin.lobraico@gmail.com>
2025-07-02 23:42:44 +0200

    feat(customers): general token

--- MSPR-BLOC4-Clients-tests/CustomWebApplicationFactory.cs
+++ MSPR-BLOC4-Clients-tests/CustomWebApplicationFactory.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
 using MSPR_bloc_4_customers.Data;
 using System.Linq;
 
@@ -11,9 +9,10 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment("Testing");
+
         builder.ConfigureServices(services =>
         {
-            // Supprimer le DbContext SQL Server
             var descriptor = services.SingleOrDefault(
                 d => d.ServiceType == typeof(DbContextOptions<ClientDBContext>));
 
@@ -22,41 +21,28 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                 services.Remove(descriptor);
             }
 
-            // Ajouter le DbContext InMemory
             services.AddDbContext<ClientDBContext>(options =>
             {
                 options.UseInMemoryDatabase("TestDb");
             });
 
-            // Configurer l'authentification fake
-            services.AddAuthentication("Test")
-                .AddScheme<Microsoft.AspNetCore.Authentication.AuthenticationSchemeOptions, FakeJwtAuthHandler>(
-                    "Test", options => { });
-
-            // Définir le schéma d'authentification par défaut
-            services.Configure<Microsoft.AspNetCore.Authentication.AuthenticationOptions>(options =>
-            {
-                options.DefaultAuthenticateScheme = "Test";
-                options.DefaultChallengeScheme = "Test";
-            });
-
-            // Créer la base et l'initialiser si besoin
             var sp = services.BuildServiceProvider();
+
             using (var scope = sp.CreateScope())
             {
-                var db = scope.ServiceProvider.GetRequiredService<ClientDBContext>();
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<ClientDBContext>();
+
                 db.Database.EnsureCreated();
 
-                // Optionnel : vider la table et ajouter des données initiales pour les tests
-                if (!db.Clients.Any())
+                db.Clients.Add(new MSPR_bloc_4_customers.Models.Client
                 {
-                    db.Clients.Add(new MSPR_bloc_4_customers.Models.Client
-                    {
-                        IdClient = 1,
-                        Nom = "Test Client",
-                    });
-                    db.SaveChanges();
-                }
+                    IdClient = 1,
+                    Nom = "Test",
+                    Prenom = "User"
+                });
+
+                db.SaveChanges();
             }
         });
     }
