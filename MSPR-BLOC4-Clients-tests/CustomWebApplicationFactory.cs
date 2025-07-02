a4217acd4e517a88a6d299eeb87ebaf150656ff0
Val-lhe-lob <valentin.lobraico@gmail.com>
2025-07-02 23:52:23 +0200

    feat(customers): general token

--- MSPR-BLOC4-Clients-tests/CustomWebApplicationFactory.cs
+++ MSPR-BLOC4-Clients-tests/CustomWebApplicationFactory.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Authentication;
 using MSPR_bloc_4_customers.Data;
 using MSPR_bloc_4_customers.Models;
 using System.Linq;
@@ -14,37 +16,47 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 
         builder.ConfigureServices(services =>
         {
+            // Remove existing DbContext registration
             var descriptor = services.SingleOrDefault(
                 d => d.ServiceType == typeof(DbContextOptions<ClientDBContext>));
-
             if (descriptor != null)
-            {
                 services.Remove(descriptor);
-            }
 
+            // Add InMemory DbContext
             services.AddDbContext<ClientDBContext>(options =>
             {
                 options.UseInMemoryDatabase("TestDb");
             });
 
+            // Register Fake Authentication
+            services.AddAuthentication("Test")
+                .AddScheme<AuthenticationSchemeOptions, FakeJwtAuthHandler>("Test", options => { });
+
+            services.Configure<AuthenticationOptions>(options =>
+            {
+                options.DefaultAuthenticateScheme = "Test";
+                options.DefaultChallengeScheme = "Test";
+            });
+
+            // Build the service provider
             var sp = services.BuildServiceProvider();
 
+            // Seed test data
             using (var scope = sp.CreateScope())
             {
                 var scopedServices = scope.ServiceProvider;
                 var db = scopedServices.GetRequiredService<ClientDBContext>();
-
                 db.Database.EnsureCreated();
 
                 db.Clients.Add(new Client
                 {
                     IdClient = 1,
-                    Nom = "Test",
-                    Prenom = "User",
+                    Nom = "Test Nom",
+                    Prenom = "Test Prenom",
+                    Adresse = "123 Rue de Test",
                     CodePostal = "75000",
                     Ville = "Paris"
                 });
-
                 db.SaveChanges();
             }
         });
